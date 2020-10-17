040976f1194da689ac2dbd8973ce7dbc6bf60f60
Abdihakim <wehelie@uwindsor.ca>
2020-10-16 22:37:10 -0400

    fixed variable

--- Landing_pg/js/app.js
+++ Landing_pg/js/app.js
@@ -71,7 +71,7 @@ function scrollToSection() {
 			const linkName = link.getAttribute('href');
 			const linkNumber = linkName.substr(-1);
 			const section = sections[linkNumber-1];
-			const top = section.getBoundingClientRect().top + window.pageYOffset - navBar.offsetHeight;
+			const top = section.getBoundingClientRect().top + window.pageYOffset - navMenu.offsetHeight;
 			window.scrollTo({
 				top,
 				behavior: 'smooth'
