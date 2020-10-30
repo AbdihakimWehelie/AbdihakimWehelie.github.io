9638a3f9bd0ef9a8bca26932cd601c610888de47
Abdihakim <wehelie@uwindsor.ca>
2020-10-30 16:31:24 -0400

    readded the express

--- fend-refresh-2019/projects/weather-journal-app/server.js
+++ fend-refresh-2019/projects/weather-journal-app/server.js
@@ -45,7 +45,7 @@ app.use(bodyParser.json());
 
 
 /* Initialize the main project folder*/
-//app.use(express.static('website'));
+app.use(express.static('website'));
 
 const port = 8000;
 /* Spin up the server*/
