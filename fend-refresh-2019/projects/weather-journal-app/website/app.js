6bc81dbaef07d04408a43b5b243af8149155736f
Abdihakim <wehelie@uwindsor.ca>
2020-10-31 01:04:59 -0400

    trying something with json

--- fend-refresh-2019/projects/weather-journal-app/website/app.js
+++ fend-refresh-2019/projects/weather-journal-app/website/app.js
@@ -121,7 +121,7 @@ const postData = async (url = '', data = {}) => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(data),
+        body: JSON.stringify(data,['date','temperature','userResponse']),
     });
     console.log(response);
     try {
